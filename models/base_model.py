268a9a476cf5622e964029c670309349d06f89aa
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-07-10 16:22:14 -0500

    change class

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-
+my_uuid = uuid()
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = str(my_uuid)
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
