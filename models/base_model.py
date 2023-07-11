61078b66e778838cc4354cf84ad40de65eeaa616
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-07-11 13:14:32 -0500

    style

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,73 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""a class BaseModel"""
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """
+        Constructor for BaseModel.
 
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            *args: Variable-length arguments.
+
+        If kwargs is not empty:
+            - Each key of the dictionary
+            is an attribute name (excluding '__class__').
+            - Each value of the dictionary is the value
+            for the corresponding attribute name.
+
+        If kwargs is empty:
+            - The 'id' attribute is assigned a unique identifier.
+            - The 'created_at' attribute is set to the current datetime.
+            - The 'updated_at' attribute is set to the current datetime.
+
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """
+        Return a string representation of the BaseModel object.
+
+        Returns:
+            str: A string representation in the format:
+                 "[<class name>] (<id>) <attribute dictionary>"
+                 """
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Update the 'updated_at' attribute with the current datetime.
+
+        """
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of the BaseModel object.
+
+        Returns:
+            dict: A dictionary containing all keys/values
+            of the instance's __dict__,
+            including '__class__', 'created_at', and 'updated_at'.
+            The 'created_at'
+            and 'updated_at' values are converted
+            to string objects in ISO format.
+
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
