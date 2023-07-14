9c7a0ced22e37fcdd66ab64d18fb21ddd0967faa
rubio91147 <rubendariofb@gmail.com>
2023-07-14 14:12:39 -0500

    update file storege

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 """
-Module Name:
-file_storage
+calss Module Name:
 
-Module Description:
-This module contains only one Class
-
-Module Classes:
-- FileStorage
-
-Module Attributes:
-- None
 """
 import json
 from models.base_model import BaseModel
@@ -23,55 +14,43 @@ from models.state import State
 from os import path
 
 
-class FileStorage():
+class FileStorage:
     """
-    This class is responsible for storing and retrieving
-    objects from a JSON file.
+    FileStorage class that manages the storage and retrieval of objects in a JSON file.
 
     Attributes:
-
-    __file_path: str - a private class attribute representing
-                 the path to the JSON file.
-    __objects: dict - a private class attribute representing
-               the objects stored in the JSON file.
+        __file_path (str): Path to the JSON file.
+        __objects (dict): Dictionary of stored objects.
     """
-    __file_path = "file.json"
-    __objects = {}
+
+    def __init__(self):
+        """
+        Initializes a new FileStorage instance.
+        """
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
         """
-        This method returns a dictionary containing all of
-        the objects stored in the JSON file.
+        Returns the dictionary of stored objects.
+
+        Returns:
+            dict: Dictionary of stored objects.
         """
         return self.__objects
 
     def new(self, obj):
         """
-        This method adds a new object to the dictionary of
-        objects stored in the JSON file.
+        Adds a new object to the dictionary of stored objects.
+
+        Args:
+            obj: Object to be added to the dictionary.
         """
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
         """
-        This method saves the dictionary of objects to the JSON file.
+        Serializes the objects to the JSON file.
         """
         objects_dict = {}
-        for key, value in self.__objects.items():
-            objects_dict[key] = value.to_dict()
-        with open(self.__file_path, "w", encoding='utf-8') as fl:
-            json.dump(objects_dict, fl, indent=4)
-
-    def reload(self):
-        """
-        This method loads the dictionary of objects from the JSON file.
-        """
-        try:
-            with open(self.__file_path, "r", encoding='utf-8') as fl:
-                json_data = json.load(fl)
-                for i in json_data.values():
-                    class_name = i["__class__"]
-                    del i["__class__"]
-                    self.new(eval(class_name)(**i))
-        except FileNotFoundError:
-            return
