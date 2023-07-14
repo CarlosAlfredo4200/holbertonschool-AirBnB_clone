88f01b01fd3d402f13894139e2342ea2c26d3242
rubio91147 <rubendariofb@gmail.com>
2023-07-14 14:26:50 -0500

    update file storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
-"""
-Models Package
-"""
-from .engine import file_storage
 
-storage = file_storage.FileStorage()
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
 storage.reload()
